ec99349cf6f6463b7bd1358a07e25b89023e3175
Jon Jagger <jon@jaggersoft.com>
2018-07-11 06:53:30 +0100

    upgraded to Ubuntu 18.04 and node 10

--- docker/install.sh
+++ docker/install.sh
@@ -3,7 +3,7 @@ set -e
 
 apt-get update
 apt-get install --yes curl
-curl --silent --location https://deb.nodesource.com/setup_8.x | bash -
+curl --silent --location https://deb.nodesource.com/setup_10.x | bash -
 apt-get remove --yes curl
 apt-get install --yes nodejs
 npm install -global coffeescript
